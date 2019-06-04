d125de8d10e38b8666656e797c3dd2356fda74b2
KimberlySwinton <kimberlyswinton@gmail.com>
2019-06-04 01:39:43 -0400

    started css

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -4,7 +4,7 @@ import './Card.css';
 const CardBanner = () => {
     return (
       <div className='cardImgContainer'>
-        <img
+        <img className = 'cardImg'
           src='https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png'
           alt='react logo'
         />
